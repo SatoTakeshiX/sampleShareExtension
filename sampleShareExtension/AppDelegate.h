6cb336424972be45669001db6ac6d14cdc73de4f
satoutakeshi <sato.takeshi1987@gmail.com>
2015-08-04 20:45:48 +0900

    Initial Commit

---
+++ sampleShareExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleShareExtension
+//
+//  Created by satoutakeshi on 2015/08/04.
+//  Copyright (c) 2015年 satoutakeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
